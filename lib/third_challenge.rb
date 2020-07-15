41aa1c1ae52de91b3adb0be13bf545a8d5dd5e0f
Milos Popovic <milos_popovic@outlook.com>
2020-07-15 02:34:14 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -3,28 +3,67 @@ def third_challenge
    :montague => {
       :patriarch => {
         #your key/value pairs here
+        attributes: [
+
+        ]
         },
       :matriarch => {
         #your key/value pairs here
+        attributes: [
+
+        ]
         },
       :hero => {
         #your key/value pairs here
+        attributes: [
+
+        ]
         },
-      :hero_friends => []
-   }, 
+      :hero_friends => [
+        friend_one: [
+          name: "Benvolio",
+          age: "17",
+          attitude: "worried"
+        ],
+        friend_two: [
+          name: "Mercutio",
+          age: "18",
+          attitude: "hot-headed"
+        ]
+      ]
+   },
    :capulet => {
       :patriarch => {
-        #your key/value pairs here
-        },
-      :matriarch => {
-        #your key/value pairs here
-        },
-      :heroine => {
-        #your key/value pairs here
-        },
-      :heroine_friends => []
-   }
+        #your key/value pairs here        #your key/value pairs here
+                attributes: [
+
+                ]
+                },
+              :matriarch => {
+                #your key/value pairs here
+                attributes: [
+
+                ]
+                },
+              :hero => {
+                #your key/value pairs here
+                attributes: [
+
+                ]
+                },
+              :heroine_friends => [
+                friend_one: [
+                  name: "Steven",
+                  age: "30",
+                  attitude: "confused"
+                ],
+                friend_two: [
+                  name: "Nurse",
+                  age: "44",
+                  attitude: "worried"
+                ]
+              ]   }
   }
 
-  
+
 end
