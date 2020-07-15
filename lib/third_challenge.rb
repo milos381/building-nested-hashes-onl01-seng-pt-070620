9d0a9eb10d42d9452a858923b7e5914cf9a7d2e6
Milos Popovic <milos_popovic@outlook.com>
2020-07-15 02:49:18 +0000

    Automatically backed up by Learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -24,7 +24,7 @@ def third_challenge
           age: "17",
           attitude: "worried"
           }
-        {
+        ,{
           name: "Mercutio",
           age: "18",
           attitude: "hot-headed"
