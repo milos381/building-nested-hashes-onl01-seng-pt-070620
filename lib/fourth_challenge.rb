fd50ea817b702a0c39a28a7310982cc303b7cb8c
Milos Popovic <milos_popovic@outlook.com>
2020-07-15 02:55:53 +0000

    Automatically backed up by Learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -6,17 +6,20 @@ def fourth_challenge
       :hero => {name: "Romeo", age: "15", status: "alive"},
       :hero_friends => [
         #your hashes here!
-        name: "Steven",
-        age: "30",
-        attitude: "confused"
-      ],
-
-        name: "Nurse",
-        age: "44",
+        {
+        name: "Benevolio",
+        age: "17",
         attitude: "worried"
+      }
+      ,
+        {
+        name: "Mercutio",
+        age: "18",
+        attitude: "hot-headed"
+      }
     ]
 
-      ]
+
    },
    :capulet => {
       :patriarch => {name: "Lord Capulet", age: "50"},
@@ -24,14 +27,17 @@ def fourth_challenge
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
         #your hashes here!
-          name: "Steven",
-          age: "30",
-          attitude: "confused"
-        ],
-
-          name: "Nurse",
-          age: "44",
-          attitude: "worried"
+        {
+        name: "Steven",
+        age: "30",
+        attitude: "confused"
+      }
+      ,
+        {
+        name: "Nurse",
+        age: "44",
+        attitude: "worried"
+      }
       ]
    }
 }
